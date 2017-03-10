2588ed18ff49d92146cf0b6c4de8840c29393421
killerjason55 <killerjason55@gmail.com>
2017-03-10 17:43:27 +0700

    Fixes Everything

--- Lab8-11.py
+++ Lab8-11.py
@@ -1,7 +1,8 @@
 from PySide.QtGui import *
 from PySide.QtCore import *
+import sys
 
-class Simple_drawing_window(QWidget):
+class Simple_drawing_window1(QWidget):
     def __init__(self):
         QWidget.__init__(self,None)
         self.setWindowTitle("Simple Drawing")
@@ -27,3 +28,14 @@ class Simple_drawing_window(QWidget):
             ])
         p.drawImage(QRect(200,100,320,320),self.rabbit)
         p.end()
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+
+    a = Simple_drawing_window1()
+
+    a.show()
+
+    app.exec_()
+
+    sys.exit()
