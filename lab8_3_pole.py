82c3d432307a5ce3ad6dabb74a7b2fee93a16d1c
tigerSI <sirapop.tiger@gmail.com>
2017-03-10 18:47:27 +0700

    Update and rename lab8_3.py to lab8_3_pole.py

--- lab8_3_pole.py
+++ lab8_3_pole.py
@@ -1,6 +1,6 @@
 from turtle import *
-class Pole:
-    def init(name, stack = [], toppos = 0, xpos = 0, ypos = 0, thickness = 30, length = 120, color = 'white'):
+class Pole():
+    def __init__(self, name, stack = [], toppos = 0, xpos = 0, ypos = 0, thickness = 30, length = 120, color = 'white'):
         self.name = name
         self.stack = stack
         self.top = toppos
@@ -15,15 +15,18 @@ class Pole:
         goto(self.posx, self.posy)
         pd()
         heading(90)
-        for i in range 2:
+        for i in range(2):
             forward(self.thick / 2)
             left(90)
             forward(length)
             left(90)
             forward(self.thick / 2)
 
-    def pushdisk(disk):
-        pass
+    def pushdisk(self, Disk):
+        self.stack.append(Disk)
+        self.top += Disk.h
 
-    def popdisk():
-        pass
+    def popdisk(self):
+        last_disk = self.stack.pop()
+        self.top -= last_disk.h
+        return last_disk
